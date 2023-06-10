dd047af3f2e6b209bf90c35b646e1d8cb0db152d
Colson <colsoncalifornication@gmail.com>
2023-06-10 18:58:48 +0545

    check existing user and password confirmation validations

--- repositories/users.js
+++ repositories/users.js
@@ -83,11 +83,4 @@ class UsersRepository {
   }
 }
 
-const test = async () => {
-  const repo = new UsersRepository('users.json');
-
-  const user = await repo.getOneBy({ email: 'cols@google.com', password: 'GoogleInc.' });
-  console.log(user);
-};
-
-test();
+module.exports = new UsersRepository('users.json');
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const usersRepo = require('./repositories/users');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,8 +20,18 @@ app.get('/', (req, res) => {
   );
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body; 
+
+  const existingUser = await usersRepo.getOneBy({ email })
+  if (existingUser) {
+    return res.send('Email already in use.');
+  }
+
+ if (password !== passwordConfirmation) {
+  return res.send('Passwords must match');
+ }
+
   res.send('Account created!!');
 });
 
