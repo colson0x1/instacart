097bbbe72a5d53016d4f49e599f147ebdda2098c
Colson <colsoncalifornication@gmail.com>
2023-06-10 15:37:33 +0545

    implement async func to update the data store based on user id

--- repositories/users.js
+++ repositories/users.js
@@ -45,13 +45,30 @@ class UsersRepository {
     const records = await this.getAll();
     return records.find((record) => record.id === id);
   }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id != id);
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id of ${id} not found.`);
+    }
+
+    Object.assign(record, attrs);
+    await this.writeAll(records);
+  }
 }
 
 const test = async () => {
   const repo = new UsersRepository('users.json');
 
-  const user = await repo.getOne('263bd36d');
-  console.log(user);
+  await repo.update('e9bf7601', { password: 'GoogleInc.' });
 };
 
 test();
