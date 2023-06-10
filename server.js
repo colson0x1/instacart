697dbc549689eae21845559df0ba9df8c2982a62
Colson <colsoncalifornication@gmail.com>
2023-06-10 06:54:26 +0545

    add custom bodyParser middleware

--- server.js
+++ server.js
@@ -17,17 +17,27 @@ app.get('/', (req, res) => {
   );
 });
 
-app.post('/', (req, res) => {
-  req.on('data', (data) => {
-    const parsed = data.toString('utf-8').split('&');
-    const formData = {};
+const bodyParser = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', (data) => {
+      const parsed = data.toString('utf-8').split('&');
+      const formData = {};
 
-    for (let pair of parsed) {
-      const [key, value] = pair.split('=');
-      formData[key] = value;
-    }
-    console.log(formData);
-  });
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+};
+
+app.post('/', bodyParser, (req, res) => {
+  console.log(req.body);
   res.send('Account created!!');
 });
 
