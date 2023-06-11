fe4107af810cde4b11cedcdf91e039c55cdae2fa
Colson <colsoncalifornication@gmail.com>
2023-06-11 06:27:23 +0545

    implement cookie session on sign up

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersRepo = require('./repositories/users');
+const cookieSession = require('cookie-session');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+  keys: ['wokdowamdkwqokdw8924jkawdkalsqlp']
+}))
 
 app.get('/', (req, res) => {
   res.send(
     `
     <div>
+    Your id is: ${req.session.userId}
       <form method="POST">
         <input name="email" type="email" placeholder='Type your email' />
         <input name="password" type='password' placeholder='Create a new password' />
@@ -21,16 +27,20 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body; 
+  const { email, password, passwordConfirmation } = req.body;
 
-  const existingUser = await usersRepo.getOneBy({ email })
+  const existingUser = await usersRepo.getOneBy({ email });
   if (existingUser) {
     return res.send('Email already in use.');
   }
 
- if (password !== passwordConfirmation) {
-  return res.send('Passwords must match');
- }
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match');
+  }
+
+  const user = await usersRepo.create({ email, password });
+
+  req.session.userId = user.id;
 
   res.send('Account created!!');
 });
