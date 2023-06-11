41e3d59ca1198c3a8c82095f4234ca49c754d3c4
Colson <colsoncalifornication@gmail.com>
2023-06-11 11:50:43 +0545

    build layout to reduce code duplications

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,15 @@
+const layout = require('../layout');
+
 module.exports = () => {
-  return `
-    <div>
+  return layout({
+    content: `
+        <div>
         <form method="POST">
-        <input name="email" type="email" placeholder='Type your email' />
-        <input name="password" type='password' placeholder='Type your password' />
-        <button>Sign In</button>
-        </form>
-    </div> 
-    `;
+            <input name="email" type="email" placeholder='Type your email' />
+            <input name="password" type='password' placeholder='Type your password' />
+            <button>Sign In</button>
+            </form>
+        </div> 
+    `,
+  });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-  return `
-    <div>
-    Your id is: ${req.session.userId}
-        <form method="POST">
-        <input name="email" type="email" placeholder='Type your email' />
-        <input name="password" type='password' placeholder='Create a new password' />
-        <input name="passwordConfirmation" type='password' placeholder='Confirm your password' />
-        <button>Sign Up</button>
-        </form>
-    </div>
-    `;
+  return layout({
+    content: `
+        <div>
+        Your id is: ${req.session.userId}
+            <form method="POST">
+            <input name="email" type="email" placeholder='Type your email' />
+            <input name="password" type='password' placeholder='Create a new password' />
+            <input name="passwordConfirmation" type='password' placeholder='Confirm your password' />
+            <button>Sign Up</button>
+            </form>
+        </div>
+    `,
+  });
 };
