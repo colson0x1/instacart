e070252131d2215a694ab9be627ec7b24fddbcae
Colson <colsoncalifornication@gmail.com>
2023-06-14 23:09:20 +0545

    display errors message for input errors during product edit

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -1,14 +1,17 @@
+const { getError } = require('../../helpers');
 const layout = require('../layout');
 
-module.exports = ({ product }) => {
-    return layout({
-        content: `
+module.exports = ({ product, errors }) => {
+  return layout({
+    content: `
           <form method="POST">
             <input name="title" value="${product.title}" />
+            ${getError(errors, 'title')}
             <input name="price" value="${product.price}" />
+            ${getError(errors, 'price')}
             <input name="image" type="file" />
             <button>Update</button>            
           </form>
-        `
-    })
-}
+        `,
+  });
+};
