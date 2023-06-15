52a569b10aa07163652799ffa839b680f990773b
Colson <colsoncalifornication@gmail.com>
2023-06-15 14:46:27 +0545

    add items to cart

--- routes/carts.js
+++ routes/carts.js
@@ -14,12 +14,25 @@ router.post('/cart/products', async (req, res) => {
     req.session.cartId = cart.id;
   } else {
     // Cart exist. Get it from the repository
-    cart = await cartsRepo.getOne(req.session.cartId)
+    cart = await cartsRepo.getOne(req.session.cartId);
   }
 
   console.log(cart);
   // Either increment quantity for existing product
   // OR add new product to items array
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    // increment quantity and save cart
+    existingItem.quantity++;
+  } else {
+    // add new product id to items array
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
 
   res.send('Product added to cart!');
 });
